3314486c00233cdbee3f4209432283f5901d0915
Trollope071 <cool.savva-varma@ya.ru>
2023-03-21 14:46:46 +0300

    Initial Commit

---
+++ AppStoreClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppStoreClone
+//
+//  Created by Савва Варма on 21.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
