a146224563e42a0c52bdc4758ab8f20c9bf7b98f
Trollope071 <cool.savva-varma@ya.ru>
2023-03-23 10:00:48 +0300

    ADD: extensions for convenience UX: add row cells in app section

---
+++ AppStoreClone/View/Apps/AppRowCell.swift
@@ -0,0 +1,46 @@
+//
+//  AppRowCell.swift
+//  AppStoreClone
+//
+//  Created by Савва Варма on 23.03.2023.
+//
+
+import UIKit
+
+class AppRowCell: UICollectionViewCell {
+    
+    let imageView = UIImageView(cornerRadius: 8)
+    
+    let nameLabel = UILabel(text: "App Name", font: .systemFont(ofSize: 16))
+    let companyLabel = UILabel(text: "Company Name", font: .systemFont(ofSize: 12))
+    
+    let getButton = UIButton(title: "GET")
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        backgroundColor = .red
+        imageView.backgroundColor = .purple
+        imageView.constrainWidth(constant: 64)
+        imageView.constrainHeight(constant: 64)
+        
+        getButton.backgroundColor = UIColor(white: 0.95, alpha: 1)
+        getButton.constrainWidth(constant: 80)
+        getButton.constrainHeight(constant: 32)
+        getButton.titleLabel?.font = .systemFont(ofSize: 16)
+        getButton.layer.cornerRadius = 32 / 2
+        
+        let stackView = UIStackView(arrangedSubviews: [imageView, VerticalStackView(arrangedSubviews: [nameLabel,companyLabel]), getButton])
+        stackView.spacing = 16
+        stackView.alignment = .center
+        
+        addSubview(stackView)
+        stackView.fillSuperview()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- AppStoreClone/View/Apps/AppsGroupCell.swift
+++ AppStoreClone/View/Apps/AppsGroupCell.swift
@@ -7,13 +7,6 @@
 
 import UIKit
 
-extension UILabel {
-    convenience init(text: String, font: UIFont) {
-        self.init(frame: .zero)
-        self.text = text
-        self.font = font
-    }
-}
 
 class AppsGroupCell: UICollectionViewCell {
     
