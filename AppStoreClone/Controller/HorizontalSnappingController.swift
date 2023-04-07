8617eb66596fdf9a0b194b067564aabaa6848f8f
Trollope071 <cool.savva-varma@ya.ru>
2023-04-07 19:07:38 +0300

    UX: snapping cells beautify

---
+++ AppStoreClone/Controller/HorizontalSnappingController.swift
@@ -0,0 +1,22 @@
+//
+//  HorizontalSnappingController.swift
+//  AppStoreClone
+//
+//  Created by Савва Варма on 07.04.2023.
+//
+
+import UIKit
+
+class HorizontalSnappingController: UICollectionViewController {
+    
+    init() {
+        let layout = BetterSnappingLayout()
+        layout.scrollDirection = .horizontal
+        super.init(collectionViewLayout: layout)
+        collectionView.decelerationRate = .fast
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
